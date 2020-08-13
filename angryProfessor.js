613712baf94d2e1b8b7e8781fe6b5a6eb34bdf51
Jules Kulcsar <kulcsar_jules@yahoo.com>
2020-08-13 22:51:08 +0200

    angry professor challenge

---
+++ angryProfessor.js
@@ -0,0 +1,14 @@
+//A Discrete Mathematics professor has a class of students.
+//Frustrated with their lack of discipline,
+//he decides to cancel class if fewer than some number of students are present when class starts.
+
+//Given the arrival time of each student and a threshhold number of attendees,
+//determine if the class is canceled.
+
+// k = cancelation threshhold
+//a = array containing arrival times (0 = on time; >0 means late; - means early)
+
+function angryProfessor(k, a) {
+  let onTime = a.filter(t => t <= 0);
+  return onTime.length >= k ? "NO" : "YES";
+}
